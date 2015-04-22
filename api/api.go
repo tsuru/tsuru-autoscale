34afb81c6cb73e654f31e7ec77c1267876995340
Andrews Medina <andrewsmedina@gmail.com>
2015-04-22 14:51:30 -0300

    api: add new action.

--- api/api.go
+++ api/api.go
@@ -14,6 +14,7 @@ import (
 func Router() http.Handler {
 	m := mux.NewRouter()
 	m.HandleFunc("/datasource", newDataSource).Methods("POST")
+	m.HandleFunc("/action", newAction).Methods("POST")
 	m.HandleFunc("/resources", serviceAdd)
 	m.HandleFunc("/resources/{name}/bind", serviceBind).Methods("POST")
 	m.HandleFunc("/resources/{name}/bind", serviceUnbind).Methods("DELETE")
