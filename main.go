16428f5bdf6130811bc8b5503732b287638ff117
Andrews Medina <andrewsmedina@gmail.com>
2015-04-23 18:10:04 -0300

    main: get port from $PORT env.

--- main.go
+++ main.go
@@ -5,16 +5,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/tsuru/tsuru-autoscale/alarm"
 	"github.com/tsuru/tsuru-autoscale/api"
 )
 
+func port() string {
+	var p string
+	if p = os.Getenv("PORT"); p != "" {
+		return p
+	}
+	return "8080"
+}
+
 func main() {
 	alarm.StartAutoScale()
 	r := api.Router()
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port()), nil))
 }
