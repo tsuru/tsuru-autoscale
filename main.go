1af83043804a450a0ea52ed58866b9f0c06199bf
Andrews Medina <andrewsmedina@gmail.com>
2015-04-28 22:30:39 -0300

    using codegangsta/cli.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/codegangsta/cli"
 	"github.com/tsuru/tsuru-autoscale/alarm"
 	"github.com/tsuru/tsuru-autoscale/api"
 )
@@ -22,9 +23,34 @@ func port() string {
 	return "8080"
 }
 
-func main() {
-	alarm.StartAutoScale()
+func runServer(c *cli.Context) {
 	r := api.Router()
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port()), nil))
+}
+
+func main() {
+	alarm.StartAutoScale()
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "binding",
+			Value: "0.0.0.0:8778",
+			Usage: "binding address",
+		},
+		cli.StringFlag{
+			Name: "mongodb-url",
+		},
+		cli.StringFlag{
+			Name: "mongodb-database",
+		},
+		cli.StringFlag{
+			Name: "mongodb-prefix",
+		},
+	}
+	app.Version = "0.0.1"
+	app.Name = "autoscale"
+	app.Usage = "autoscale api"
+	app.Action = runServer
+	app.Run(os.Args)
 }
