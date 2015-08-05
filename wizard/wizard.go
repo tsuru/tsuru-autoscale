19a0fb81f1f874aa034bafc4c453d873c0c9495c
Andrews Medina <andrewsmedina@gmail.com>
2015-08-05 11:22:30 -0300

    wizard: add scale types

---
+++ wizard/wizard.go
@@ -0,0 +1,19 @@
+// Copyright 2015 tsuru-autoscale authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wizard
+
+type autoscale struct {
+	scaleUp   scaleAction
+	scaleDown scaleAction
+	minUnits  int
+}
+
+type scaleAction struct {
+	metric   string
+	operator string
+	value    string
+	step     int
+	waitTime int
+}
