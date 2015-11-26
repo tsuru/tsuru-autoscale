fc43e6be86771a26a95e9b5d91d28a131730cc32
Andrews Medina <andrewsmedina@gmail.com>
2015-11-26 14:23:42 -0200

    log: add Error and Errorf method to log proxy

--- log/log.go
+++ log/log.go
@@ -23,6 +23,14 @@ func (l *Logger) Printf(format string, v ...interface{}) {
 	l.lg.Printf(format, v)
 }
 
+func (l *Logger) Error(v ...interface{}) {
+	l.lg.Error(v)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.lg.Errorf(format, v)
+}
+
 func New() *Logger {
 	return &Logger{lg: log.New(os.Stdout, "[autoscale] ", 0)}
 }
