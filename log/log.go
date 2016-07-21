8dfd572d7224aab724f8fa19da8678d4c8dc446b
Andrews Medina <andrewsmedina@gmail.com>
2016-07-21 00:06:14 -0300

    log: fix lint

--- log/log.go
+++ log/log.go
@@ -19,27 +19,33 @@ func init() {
 
 var lg *Logger
 
+// Logger represents a logger
 type Logger struct {
 	lg *log.Logger
 }
 
+// Print writes an info in the log
 func (l *Logger) Print(v ...interface{}) {
 	l.lg.Print(v)
 }
 
+// Printf writes an info with format in the log
 func (l *Logger) Printf(format string, v ...interface{}) {
 	l.lg.Printf(format, v...)
 }
 
+// Error writes an error in the log
 func (l *Logger) Error(err error) {
 	raven.CaptureError(err, nil)
 	l.Print(err)
 }
 
+// New returns a new Logger
 func New() *Logger {
 	return &Logger{lg: log.New(os.Stdout, "[autoscale] ", 0)}
 }
 
+// Log returns the Logger
 func Log() *Logger {
 	if lg == nil {
 		lg = New()
