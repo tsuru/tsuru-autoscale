c850e3b4452748ceff06e564438f243233420f7a
Andrews Medina <andrewsmedina@gmail.com>
2016-10-10 19:00:13 -0300

    web: add wizard list

--- web/handlers.go
+++ web/handlers.go
@@ -7,11 +7,26 @@ package web
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/tsuru/tsuru-autoscale/wizard"
 )
 
 func wizardHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("web/templates/wizard.html")
-	t.Execute(w, nil)
+	t, err := template.ParseFiles("web/templates/wizard.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	wizards, err := wizard.FindBy(nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, wizards)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
