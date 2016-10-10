43d562c8b78e751706b6bc0a262cf4bcc26618eb
Andrews Medina <andrewsmedina@gmail.com>
2016-10-10 14:11:51 -0300

    web: remove runserver and expose Router method

--- web/web.go
+++ web/web.go
@@ -5,16 +5,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 )
 
-func router() http.Handler {
+// Router return a http.Handler with all web routes
+func Router() http.Handler {
 	m := mux.NewRouter()
 	m.HandleFunc("/", indexHandler).Methods("GET")
 	m.HandleFunc("/wizard", wizardHandler).Methods("GET")
@@ -23,21 +21,4 @@ func router() http.Handler {
 	n.Use(negroni.NewLogger())
 	n.UseHandler(m)
 	return n
-}
-
-func port() string {
-	var p string
-	if p = os.Getenv("PORT"); p != "" {
-		return p
-	}
-	return "8080"
-}
-
-func runServer() {
-	http.Handle("/", router())
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port()), nil))
-}
-
-func main() {
-	runServer()
 }
