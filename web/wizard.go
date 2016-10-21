1f5724fa67b600497a7e1db9b3b0b66be514645d
Andrews Medina <andrewsmedina@gmail.com>
2016-10-21 19:05:03 -0200

    web: add handler to add wizard

--- web/wizard.go
+++ web/wizard.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/ajg/form"
 	"github.com/gorilla/mux"
 	"github.com/tsuru/tsuru-autoscale/wizard"
 )
@@ -35,4 +36,32 @@ func wizardHandler(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 	return t.Execute(w, wizards)
+}
+
+func wizardAdd(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			return err
+		}
+		var w wizard.AutoScale
+		d := form.NewDecoder(nil)
+		d.IgnoreCase(true)
+		d.IgnoreUnknownKeys(true)
+		err = d.DecodeValues(&w, r.Form)
+		if err != nil {
+			return err
+		}
+		err = wizard.New(&w)
+		if err != nil {
+			return err
+		}
+		http.Redirect(w, r, "/web/wizard", 302)
+		return nil
+	}
+	t, err := template.ParseFiles("web/templates/wizard/add.html")
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, nil)
 }
