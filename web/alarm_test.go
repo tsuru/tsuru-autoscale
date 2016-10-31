1dcc9419bbef1835b034f918f197bc56d49d876f
Andrews Medina <andrewsmedina@gmail.com>
2016-10-31 18:31:58 -0200

    web: add test for alarm disable

--- web/alarm_test.go
+++ web/alarm_test.go
@@ -25,3 +25,17 @@ func (s *S) TestAlarmEnable(c *check.C) {
 	c.Assert(err, check.IsNil)
 	c.Assert(a.Enabled, check.Equals, true)
 }
+
+func (s *S) TestAlarmDisable(c *check.C) {
+	a := &alarm.Alarm{Name: "myalarm", Enabled: true}
+	err := alarm.NewAlarm(a)
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/alarm/myalarm/disable", nil)
+	c.Assert(err, check.IsNil)
+	server(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusFound)
+	a, err = alarm.FindAlarmByName("myalarm")
+	c.Assert(err, check.IsNil)
+	c.Assert(a.Enabled, check.Equals, false)
+}
