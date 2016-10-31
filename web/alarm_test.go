6d9cf493dceb1b223e1d7e453fd3cbeefbc260bc
Andrews Medina <andrewsmedina@gmail.com>
2016-10-31 18:31:14 -0200

    web: add test for alarm enable

---
+++ web/alarm_test.go
@@ -0,0 +1,27 @@
+// Copyright 2016 tsuru-autoscale authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package web
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/tsuru/tsuru-autoscale/alarm"
+	"gopkg.in/check.v1"
+)
+
+func (s *S) TestAlarmEnable(c *check.C) {
+	a := &alarm.Alarm{Name: "myalarm", Enabled: false}
+	err := alarm.NewAlarm(a)
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/alarm/myalarm/enable", nil)
+	c.Assert(err, check.IsNil)
+	server(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusFound)
+	a, err = alarm.FindAlarmByName("myalarm")
+	c.Assert(err, check.IsNil)
+	c.Assert(a.Enabled, check.Equals, true)
+}
