838a0e668194a35e83875c9e9b78cc1e7184a684
Andrews Medina <andrewsmedina@gmail.com>
2016-11-03 11:34:02 -0200

    web: add test for enable wiard

--- web/wizard_test.go
+++ web/wizard_test.go
@@ -24,3 +24,39 @@ func (s *S) TestWizardRemove(c *check.C) {
 	_, err = wizard.FindByName(a.Name)
 	c.Assert(err, check.NotNil)
 }
+
+func (s *S) TestWizardEnable(c *check.C) {
+	scaleUp := wizard.ScaleAction{
+		Metric:   "cpu",
+		Operator: ">",
+		Step:     "1",
+		Value:    "10",
+		Wait:     50,
+	}
+	scaleDown := wizard.ScaleAction{
+		Metric:   "cpu",
+		Operator: "<",
+		Step:     "1",
+		Value:    "2",
+		Wait:     50,
+	}
+	a := wizard.AutoScale{
+		Name:      "new",
+		ScaleUp:   scaleUp,
+		ScaleDown: scaleDown,
+		Process:   "web",
+		MinUnits:  2,
+	}
+	recorder := httptest.NewRecorder()
+	err := wizard.New(&a)
+	c.Assert(err, check.IsNil)
+	err = a.Disable()
+	c.Assert(err, check.IsNil)
+	request, err := http.NewRequest("GET", "/wizard/new/enable", nil)
+	c.Assert(err, check.IsNil)
+	server(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusFound)
+	r, err := wizard.FindByName(a.Name)
+	c.Assert(err, check.IsNil)
+	c.Assert(r.Enabled(), check.Equals, true)
+}
